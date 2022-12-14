89a45f5bb14e5de2b99acd62b871e4b9f16f730c
Jiayi Louise Zhou <louisezhou2004@gmail.com>
2022-12-14 10:27:21 +0800

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import os
 
+
+
+def main ():
+    csv_file_name = input ("The name of the csv file:")
+    
+    df = pd.read_csv()
+    
+
+
+if __name__ == '__main__' :
+    direction = input ("The file diretory of the csv file:")
+    os.getcwd()
+    os.chdir(direction)
+
+    main()
